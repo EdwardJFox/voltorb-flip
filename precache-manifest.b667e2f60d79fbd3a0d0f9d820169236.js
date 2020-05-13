ef4091893d55d7072cb3d7d97d04e5dc2d8110a0
EdwardJFox <edwardjfox@outlook.com>
2020-05-13 01:24:58 +0000

    Deploying to gh-pages from master @ 2b7ba0e2194089ecee7aceec85771d419d3ba5c1 🚀

---
+++ precache-manifest.b667e2f60d79fbd3a0d0f9d820169236.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8689c358acc10f9507b97c61f3d673e0",
+    "url": "/voltorb-flip/index.html"
+  },
+  {
+    "revision": "32bff39a935a9e5ee482",
+    "url": "/voltorb-flip/static/css/main.be234031.chunk.css"
+  },
+  {
+    "revision": "ed13aed2497ca4abcce7",
+    "url": "/voltorb-flip/static/js/2.e44c1c31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/voltorb-flip/static/js/2.e44c1c31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32bff39a935a9e5ee482",
+    "url": "/voltorb-flip/static/js/main.cd88f98b.chunk.js"
+  },
+  {
+    "revision": "0f5c87014e0107a880a1",
+    "url": "/voltorb-flip/static/js/runtime-main.2d2e4099.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e91ce0b367acfa7cc27f9c2769316b06.js"
+  "/voltorb-flip/precache-manifest.b667e2f60d79fbd3a0d0f9d820169236.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/voltorb-flip/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
